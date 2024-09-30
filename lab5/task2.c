fbc5b41e7c603b387acc4846dcc02e00ff69ccb8
Saad Tauseef <126574907+DAAS69@users.noreply.github.com>
2024-10-01 00:10:29 +0500

    task2.c

--- lab5/task2.c
+++ lab5/task2.c
@@ -1,1 +1,10 @@
-// i cant understand the question.
+#include <stdio.h>
+#include <stdbool.h>
+bool sleep_in(bool week_day, bool vacation) {
+    return !week_day || vacation;
+}
+int main() {
+printf("sleep_in(false, false) → %s\n", sleep_in(false, false) ? "true" : "false");
+printf("sleep_in(true, false) → %s\n", sleep_in(true, false) ? "true" : "false");
+printf("sleep_in(false, true) → %s\n", sleep_in(false, true) ? "true" : "false");
+}
