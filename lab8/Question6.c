08237933e7d54dc44d1c9a3d06215e5bbd02f952
Saad Tauseef <126574907+DAAS69@users.noreply.github.com>
2024-10-27 14:36:03 +0500

    Question6.c

--- lab8/Question6.c
+++ lab8/Question6.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
-int main() {
-    int i,j,k; 
-    int arr[5][5];
+int factorial(int n) {
+      if(n == 0  || n==1)
+      return 1;
+    long factorial = 1;
+    for (int i = 2; i <= n; i++)
+        factorial = factorial * i;
+    return factorial;
+}
 
+int nCr(int n, int r) {
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+int main() {
+    int j,i,k;
     for (i=0;i<5;i++) {
         for(k=0;k<=5-i;k++){
             printf(" ");
 
         }
-        for (j=0;j<=i;j++) {
-            if (j==0 || j==i) {
-                arr[i][j] = 1;
-            } 
-            else {
-                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
-            }
-            printf("%d ", arr[i][j]);
+        for(j=0;j<=i;j++){
+            int n=i;
+            printf("%d ", nCr(n, j));
+            n++;
         }
         printf("\n");
     }
-
 }
